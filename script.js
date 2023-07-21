e9867413cf895a186bfc86dd87ddd39d018cf937
Adé Obayomi <86984626+Elapache98@users.noreply.github.com>
2023-07-20 22:58:54 -0400

    logic + navigation fixed

--- script.js
+++ script.js
@@ -1,56 +1,27 @@
-function personalizeApp() {
+function updateTitle() {
     const userName = document.getElementById("userId").value.trim();
-    const nameUpdate = document.getElementById("nameUpdate");
+
     if (userName !== "") {
         document.getElementById("appName").textContent = `${userName}'s To-Do List`;
         document.getElementById("nameUpdate").style.display = "none";
+        document.getElementById("userId").value = "";
 
-
-
-
+        // Save the 'userName' to Local Storage
+        localStorage.setItem('userName', userName);
     }
-    document.getElementById("userId").value = "";
 }
-personalizeApp();
-
-
-const button1 = document.getElementById("button1");
-
-
-function applyNameChange() {
-    button1.addEventListener("click", personalizeApp);
-}
-applyNameChange();
-
-// Retrieve the data from Local Storage
-const userName = localStorage.getItem('userName');
-const todoListData = JSON.parse(localStorage.getItem('todoListData'));
 
 // Update the app title with the user's name when the page loads
+const userName = localStorage.getItem('userName');
 if (userName) {
     document.getElementById("appName").innerText = `${userName}'s To-Do List`;
 }
 
-// Function to update the app title when the user submits the name
-function updateTitle() {
-    const name = document.getElementById("nameInput").value.trim();
+// Apply the 'updateTitle' function when the 'button1' is clicked
+const button1 = document.getElementById("button1");
+button1.addEventListener("click", updateTitle);
 
-    if (name) {
-        document.getElementById("appName").innerText = `${name}'s To-Do List`;
-    }
-
-    // Save the 'name' to Local Storage
-    localStorage.setItem('userName', name);
-}
-
-function navigateToNextPage() {
-    const name = document.getElementById("nameInput").value.trim();
-
-    if (name !== "") {
-        // Save the 'name' to Local Storage
-        localStorage.setItem('userName', name);
-    }
-
+function goToNextPage() {
     // Navigate to "next.html"
     window.location.href = 'next.html';
 }
